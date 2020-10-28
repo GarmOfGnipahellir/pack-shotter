dcac5b00df01fbd680dbd1f2528fd108ec294ffa
Henrik Melsom <melsom.henrik@gmail.com>
2020-10-29 00:45:38 +0100

    basic nodes functionality

--- __init__.py
+++ __init__.py
@@ -37,7 +37,7 @@ class PackShotterImageVariationsNode(PackShotterNode):
         subtype='DIR_PATH', name="Folder", description="The folder which contains the image variations")
 
     def init(self, context):
-        self.inputs.new('NodeSocketVirtual', "Input")
+        # self.inputs.new('NodeSocketVirtual', "Input")
         self.outputs.new('NodeSocketVirtual', "Output")
 
     def draw_buttons(self, context, layout):
@@ -79,13 +79,6 @@ node_categories = [
 ]
 
 
-class PackShotterVariation(bpy.types.PropertyGroup):
-    image: bpy.props.PointerProperty(type=bpy.types.Image, name="Image",
-                                     description="The image that wil have it's file changed for different variations")
-    folder: bpy.props.StringProperty(
-        subtype='DIR_PATH', name="Folder", description="The folder which contains the image variations")
-
-
 class PackShotterRender(bpy.types.Operator):
     bl_idname = "packshotter.render"
     bl_label = "Render"
@@ -95,30 +88,21 @@ class PackShotterRender(bpy.types.Operator):
     def invoke(self, context, event):
         self.node = context.node
 
-        current_node = self.node
-        while current_node.inputs[0] != None and len(current_node.inputs[0].links) > 0:
-            current_node = current_node.inputs[0].links[0].from_node
-
-            print(current_node)
-        
         # for each variation in input
         #   change variation setting
         #   for each variation in variation input
         #       change variation setting
         #
         #       ...
-        # 
+        #
         #           render image
 
-
-        return {'FINISHED'}
         return self.execute(context)
 
     def execute(self, context):
-        image = context.scene.packshotter_variation.image
+        input_node = self.node.inputs[0].links[0].from_node
 
-        folder_abspath = bpy.path.abspath(
-            context.scene.packshotter_variation.folder)
+        folder_abspath = bpy.path.abspath(input_node.folder)
 
         dir_content = os.listdir(folder_abspath)
 
@@ -128,7 +112,7 @@ class PackShotterRender(bpy.types.Operator):
             file_relpath = bpy.path.relpath(os.path.join(folder_abspath, file))
 
             context.scene.render.filepath = f"//..\\Render\\{blend_name}_{file_name}.png"
-            image.filepath = file_relpath
+            input_node.image.filepath = file_relpath
             bpy.ops.render.render(write_still=True)
 
             print("done", file_name, file_relpath)
@@ -136,35 +120,17 @@ class PackShotterRender(bpy.types.Operator):
         return {'FINISHED'}
 
 
-class PackShotterPanel(bpy.types.Panel):
-    bl_idname = "PACKSHOTTER_PT_PackShotter"
-    bl_label = "Pack Shotter"
-    bl_space_type = "NODE_EDITOR"
-    bl_region_type = "UI"
-    bl_category = "Pack Shotter"
-
-    def draw(self, context):
-        self.layout.prop(bpy.context.scene.packshotter_variation, 'image')
-        self.layout.prop(bpy.context.scene.packshotter_variation, 'folder')
-        self.layout.operator(PackShotterRender.bl_idname)
-
-
 classes = (
     PackShotterNodeTree,
     PackShotterImageVariationsNode,
     PackShotterRenderNode,
-    PackShotterVariation,
     PackShotterRender,
-    PackShotterPanel,
 )
 
 
 def register():
     for c in classes:
         bpy.utils.register_class(c)
-
-    bpy.types.Scene.packshotter_variation = bpy.props.PointerProperty(
-        type=PackShotterVariation)
 
     nodeitems_utils.register_node_categories(
         "PACKSHOTTER_NODES", node_categories)
