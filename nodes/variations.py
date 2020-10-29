567fe3e82991652ac6c682b68f683199fdb2f879
Henrik Melsom <henrik.melsom@tarvalley.com>
2020-10-29 17:52:32 +0100

    made variations chainable

--- nodes/variations.py
+++ nodes/variations.py
@@ -1,10 +1,31 @@
+import os
 import bpy
 import nodeitems_utils
 from .common import PackShotterNode, PackShotterNodeCategory
 
 
 class PackShotterVariationsNode(PackShotterNode):
-    pass
+    def __iter__(self):
+        self._current = 0
+        self._num_variations = self.get_num_variations()
+        return self
+
+    def __next__(self):
+        if self._current < self._num_variations:
+            index = self._current
+            self._current += 1
+            return index
+        else:
+            raise StopIteration
+
+    def apply_variation(self, index):
+        raise NotImplementedError
+
+    def get_variation_name(self, index):
+        raise NotImplementedError
+
+    def get_num_variations(self):
+        return 0
 
 
 class PackShotterImageVariationsNode(PackShotterVariationsNode):
@@ -19,12 +40,23 @@ class PackShotterImageVariationsNode(PackShotterVariationsNode):
         subtype='DIR_PATH', name="Folder", description="The folder which contains the image variations")
 
     def init(self, context):
-        # self.inputs.new('NodeSocketVirtual', "Input")
+        self.inputs.new('NodeSocketVirtual', "Input")
         self.outputs.new('NodeSocketVirtual', "Output")
 
     def draw_buttons(self, context, layout):
         layout.prop(self, "image")
         layout.prop(self, "folder")
+
+    def apply_variation(self, index):
+        file = os.listdir(bpy.path.abspath(self.folder))[index]
+        self.image.filepath = os.path.join(self.folder, file)
+
+    def get_variation_name(self, index):
+        file = os.listdir(bpy.path.abspath(self.folder))[index]
+        return bpy.path.display_name(file)
+
+    def get_num_variations(self):
+        return len(os.listdir(bpy.path.abspath(self.folder)))
 
 
 category = PackShotterNodeCategory("VARIATIONS", "Variations", items=[
--- operators/render.py
+++ operators/render.py
@@ -6,42 +6,35 @@ class PackShotterRender(bpy.types.Operator):
     bl_idname = "packshotter.render"
     bl_label = "Render"
 
-    #node: nodes.output.PackShotterRenderNode
-
     def invoke(self, context, event):
         self.node = context.node
-
-        # for each variation in input
-        #   change variation setting
-        #   for each variation in variation input
-        #       change variation setting
-        #
-        #       ...
-        #
-        #           render image
-
         return self.execute(context)
 
     def execute(self, context):
         input_node = self.node.inputs[0].links[0].from_node
 
-        folder_abspath = bpy.path.abspath(input_node.folder)
-
-        dir_content = os.listdir(folder_abspath)
-
-        for file in dir_content:
-            blend_name = bpy.path.display_name(bpy.data.filepath)
-            file_name = bpy.path.display_name(file)
-            file_relpath = bpy.path.relpath(os.path.join(folder_abspath, file))
-
-            context.scene.render.filepath = os.path.join(
-                self.node.folder, f"{blend_name}_{file_name}.png")
-            input_node.image.filepath = file_relpath
-            bpy.ops.render.render(write_still=True)
-
-            print("done", file_name, file_relpath)
+        self._render_recursive(input_node, [])
 
         return {'FINISHED'}
+
+    def _render_recursive(self, variation_node, names):
+        try:
+            input_node = variation_node.inputs[0].links[0].from_node
+        except Exception:
+            input_node = None
+
+        for i in variation_node:
+            variation_node.apply_variation(i)
+            name = variation_node.get_variation_name(i)
+            new_names = names.copy()
+            new_names.append(name)
+            if input_node is not None:
+                self._render_recursive(input_node, new_names)
+            else:
+                blend_name = bpy.path.display_name(bpy.data.filepath)
+                bpy.context.scene.render.filepath = os.path.join(
+                    self.node.folder, f"{blend_name}_{'_'.join(new_names)}.png")
+                bpy.ops.render.render(write_still=True)
 
 
 REGISTER_CLASSES = (PackShotterRender,)
