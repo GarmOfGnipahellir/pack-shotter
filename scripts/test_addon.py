019a90caf30b41de3c4264fc7f6eb0fbed79dc02
Henrik Melsom <melsom.henrik@gmail.com>
2020-10-31 04:20:39 +0100

    testing failing test again

--- scripts/test_addon.py
+++ scripts/test_addon.py
@@ -1,2 +1,16 @@
-import blender_addon_tester as BAT
-BAT.test_blender_addon(addon_path="pack-shotter", blender_revision="2.90")
+import sys
+
+try:
+    import blender_addon_tester as BAT
+except Exception as e:
+    print(e)
+    sys.exit(1)
+
+try:
+    exit_val = BAT.test_blender_addon(
+        addon_path="pack-shotter", blender_revision="2.90")
+except Exception as e:
+    print(e)
+    exit_val = 1
+
+sys.exit(exit_val)
