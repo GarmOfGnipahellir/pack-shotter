b518978da7dacecd95b029699ae9a43f1c558386
Henrik Melsom <melsom.henrik@gmail.com>
2020-10-31 04:26:34 +0100

    fix failing test

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,7 +8,7 @@ def bpy_module(cache):
 
 
 def test_versionID_pass(bpy_module):
-    expect_version = (0, 2, 0)
+    expect_version = (0, 1, 0)
     return_version = get_version(bpy_module)
     assert expect_version == return_version
 
